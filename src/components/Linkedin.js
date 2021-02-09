becc27e4d5449f8c68981a6e8ad95fc355a9ac19
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2021-02-08 21:34:31 -0600

    creates Linkedin component

---
+++ src/components/Linkedin.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function Linkedin() {
+  return (
+    <div class='container mt-3'>
+      <div class='row'>
+        <div class='col-4'>
+          <div
+            class='LI-profile-badge'
+            data-version='v1'
+            data-size='medium'
+            data-locale='en_US'
+            data-type='vertical'
+            data-theme='dark'
+            data-vanity='paul-kirkpatrick-ba2998160'
+          >
+            <a
+              class='LI-simple-link'
+              href='https://www.linkedin.com/in/paul-kirkpatrick-ba2998160?trk=profile-badge'
+            >
+              My Linkedin
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
