8b8c03b57b2fb1fcb59efc074525e0543f7bf586
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2021-02-08 21:35:05 -0600

    creates footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+<footer class='footer justify-content-center'>
+  <div class='container-fluid'>
+    <div class='row'>
+      <div class='col-12 text-center'>
+        <p>Made by Paul Kirkpatrick</p>
+      </div>
+    </div>
+  </div>
+</footer>;
