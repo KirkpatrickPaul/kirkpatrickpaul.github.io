7a8d92f2cbb1c8bccb391359db8c5bb9d4e06a7b
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2021-02-10 19:03:49 -0600

    creates About page and changes LinkedIn component to fix class to className

--- src/components/Linkedin.js
+++ src/components/Linkedin.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Linkedin() {
   return (
-    <div class='container mt-3'>
-      <div class='row'>
-        <div class='col-4'>
+    <div className='container mt-3'>
+      <div className='row'>
+        <div className='col-4'>
           <div
-            class='LI-profile-badge'
+            className='LI-profile-badge'
             data-version='v1'
             data-size='medium'
             data-locale='en_US'
@@ -15,7 +15,7 @@ export default function Linkedin() {
             data-vanity='paul-kirkpatrick-ba2998160'
           >
             <a
-              class='LI-simple-link'
+              className='LI-simple-link'
               href='https://www.linkedin.com/in/paul-kirkpatrick-ba2998160?trk=profile-badge'
             >
               My Linkedin
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,2 +1,56 @@
 import React from 'react';
-import
+import LinkedIn from '../components/Linkedin';
+
+export default function About() {
+  return (
+    <div>
+      <div className='container' id='bio-container'>
+        <img
+          src='./Assets/Paul Kirkpatrick.jpg'
+          className='img-fluid float-left'
+          alt='Picture of Paul Kirkpatrick'
+        />
+        <p>
+          <inline className='first-name' style='color: #9cb700'>
+            Paul Kirkpatrick{' '}
+          </inline>
+          has lived in Wichita, KS since 2001 and enjoys building computers,
+          gaming, and ultimate frisbee. Paul has been interested in coding since
+          high school, but has only recently formalized his education. He lives
+          with his wonderful wife and three children and plays Bass Guitar at
+          his church. He was born in Newton, KS where he grew up.
+        </p>
+        <a
+          href='./Assets/Paul Kirkpatrick Resume 2020.pdf'
+          className='btn m-4'
+          download='Paul Kirkpatrick Resume'
+        >
+          Download my Résumé
+        </a>
+        <button
+          className='btn my-4'
+          type='button'
+          data-toggle='collapse'
+          data-target='#Resume'
+          aria-expanded='false'
+          aria-controls='Resume'
+        >
+          See my Résumé
+        </button>
+      </div>
+      <div className='container'>
+        <div className='collapse' id='Resume'>
+          <div className='card card-body'>
+            <img
+              className='img-fluid'
+              src='./Assets/Paul Kirkpatrick Resume 2020.jpg'
+              alt='Paul Kirkpatrick Resume'
+            />
+          </div>
+        </div>
+      </div>
+      <div className='clearfix'></div>
+      <LinkedIn />
+    </div>
+  );
+}
