4664405da457e9fc7622994a1668d863f71c6295
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2021-02-14 00:54:51 -0600

    trying new deploy

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavItem.css';
 
 export default function NavItem({ active, href, children }) {
   return (
     <li className='nav-item'>
-      <a className={'nav-link ' + active ? 'active' : 'inactive'} href={href}>
+      <Link to={href} className={'nav-link ' + active ? 'active' : 'inactive'}>
         {children}
-      </a>
+      </Link>
     </li>
   );
 }
